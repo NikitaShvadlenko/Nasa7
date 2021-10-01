3ca89518250892b62ebe191e90f1ab69b2d85f8c
Nikita <Tomskrenta@mail.ru>
2021-10-02 11:15:51 +1300

    Set up request to Nasa
    Can't decode

--- Nasa7/PictureCell.swift
+++ Nasa7/PictureCell.swift
@@ -8,7 +8,10 @@
 import UIKit
 import SnapKit
 import Moya
-
+protocol PictureCellDelegate: AnyObject {
+    // Зачем Этот протокол?
+    func pictureCell(_ pictureCell: PictureCell, needsUpdateWith closure:() -> Void)
+}
 class PictureCell: UITableViewCell {
     private lazy var imageOfTheWeek: UIImageView = {
         let imageOfTheWeek = UIImageView()
@@ -26,6 +29,7 @@ class PictureCell: UITableViewCell {
             }
         }
     }
+    private weak var delegate: PictureCellDelegate?
     // Откуда NASA image route Берет данные OpenNasaRoute?
     let imageProvider = MoyaProvider<NasaImageRoute>()
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -35,6 +39,40 @@ class PictureCell: UITableViewCell {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    override func prepareForReuse() {
+        imageOfTheWeek.image = nil
+        aspectRatioConstraint = nil
+    }
+    func configure(model: ApodModel, delegate: PictureCellDelegate) {
+        self.delegate = delegate
+        loadImage(url: model.url)
+    }
+    func loadImage(url: URL) {
+        imageProvider.request(.image(url: url)) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case let .success(response):
+                do {
+                    let image = try response.mapImage()
+                    // Зачем этот код?
+                    self.delegate?.pictureCell(self, needsUpdateWith: {
+                        [weak self] in
+                        guard let self = self else {return}
+                    })
+                    self.imageOfTheWeek.image = image
+                    let aspectRatio = image.size.height / image.size.width
+                    // swiftlint:disable:next line_length
+                    let aspectRatioConstraint = self.imageOfTheWeek.heightAnchor.constraint(equalTo: self.imageOfTheWeek.widthAnchor, multiplier: aspectRatio)
+                    self.aspectRatioConstraint = aspectRatioConstraint
+                    self.imageOfTheWeek.image = image
+                } catch {
+                    print(error)
+                }
+            case let .failure(error):
+                print(error)
+            }
+        }
+    }
 }
 
 private extension PictureCell {
@@ -42,27 +80,6 @@ private extension PictureCell {
         contentView.addSubview(imageOfTheWeek)
         imageOfTheWeek.snp.makeConstraints { make in
             make.edges.equalToSuperview()
-            func loadImage(url: URL) {
-                imageProvider.request(.image(url: url)) { [weak self] result in
-                    guard let self = self else { return }
-                    switch result {
-                    case let .success(response):
-                        do {
-                            let image = try response.mapImage()
-                            self.imageOfTheWeek.image = image
-                            let aspectRatio = image.size.height / image.size.width
-                            // swiftlint:disable:next line_length
-                            let aspectRatioConstraint = self.imageOfTheWeek.heightAnchor.constraint(equalTo: self.imageOfTheWeek.widthAnchor, multiplier: aspectRatio)
-                            self.aspectRatioConstraint = aspectRatioConstraint
-                        }
-                        catch {
-                            print(error)
-                        }
-                    case let .failure(error):
-                        print(error)
-                    }
-                }
-            }
         }
     }
 }
--- Nasa7/ViewController.swift
+++ Nasa7/ViewController.swift
@@ -7,19 +7,23 @@
 
 import UIKit
 import SnapKit
+import Moya
 
 class ViewController: UIViewController {
+    let nasaProvider = MoyaProvider <OpenNasaRoute>()
+    var apodModels: [ApodModel] = []
     private lazy var tableView: UITableView = {
         let tableView = UITableView()
         tableView.backgroundColor = .blue
         tableView.rowHeight = UITableView.automaticDimension
         tableView.dataSource = self
-        tableView.register(PictureCell.self, forCellReuseIdentifier: "PictureCell")
+        tableView.register(PictureCell.self, forCellReuseIdentifier: "\(PictureCell.self)")
         return tableView
        }()
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
+        fetchData()
     }
     private func setupViews() {
         tabBarItem.image = UIImage(systemName: "list.bullet")
@@ -30,18 +34,45 @@ class ViewController: UIViewController {
             make.edges.equalToSuperview()
         }
     }
+    func fetchData() {
+        nasaProvider.request(.apod) {[weak self] result in
+            switch result {
+            case let .success(response):
+            do {
+                let apodModels = try response.map([ApodModel].self)
+                self?.apodModels = apodModels
+                self?.tableView.reloadData()
+            } catch {
+                print(error)
+            }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
 // MARK: - TableViewDataSource
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        6
+        return apodModels.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PictureCell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "\(PictureCell.self)", for: indexPath) as? PictureCell
+        let model = apodModels[indexPath.row]
+        cell?.configure(model: model, delegate: self)
         guard let safeCell = cell else {
             fatalError("Can not deque Cell")
         }
         return safeCell
+    }
+}
+// MARK: - PictureCellDelegate
+// Зачем?
+extension ViewController: PictureCellDelegate {
+    func pictureCell(_ pictureCell: PictureCell, needsUpdateWith closure: () -> Void) {
+        tableView.beginUpdates()
+        closure()
+        tableView.endUpdates()
     }
 }
